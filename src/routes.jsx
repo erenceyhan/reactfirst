54e8177429113810506bb9375c287a82becad3fb
Eren <erenceyhann@gmail.com>
2024-03-08 22:59:08 +0300

    2

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,10 @@
 import { useRoutes } from 'react-router-dom';
 import About from './pages/about/About';
 import Contact from './pages/contact/Contact';
-
-
+import Education from './pages/education/Education';
+import Experience from './pages/experience/Experience';
+import Home from './pages/home/Home';
+import Project from './pages/project/Project';
 
   
 export default function Router() {
@@ -10,5 +12,9 @@ export default function Router() {
 return useRoutes([
     { path: '/about', element: <About /> },
     { path: '/contact', element: <Contact /> },
+    { path: '/education', element: <Education /> },
+    { path: '/experience', element: <Experience /> },
+    { path: '/home', element: <Home /> },
+    { path: '/project', element: <Project /> },
   ]);
 }
